7123adfd721bef84129d4ae09403e8989373778c
Yoonsub <yoonsub90@gmail.com>
2018-10-31 16:20:45 -0400

    blog pictures

--- foreach.js
+++ foreach.js
@@ -1,7 +1,15 @@
 var exampleArray = [0,1,2,3,4,5]
 
-for(let i = 0; i < exampleArray.length; i++){
-    console.log(exampleArray[i])
-}
+exampleArray.forEach(number => console.log(number));
 
-// exampleArray.forEach(number => console.log(number));
+
+
+
+
+
+for(let i = 0; i < exampleArray.length; i++){
+
+    console.log(exampleArray[i])  <----- This is equal to (number => console.log(number));
+
+
+}
